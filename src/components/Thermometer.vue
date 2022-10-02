5e3938edf8d4c5539c8c4b336778004db769753c
oswi <oswaldo.adrian@live.com.mx>
2022-10-02 16:32:18 -0500

    reading sensor data

--- src/components/Thermometer.vue
+++ src/components/Thermometer.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import sensorApi from '../api/sensorApi';
-const data = ref({
-  temperature: 0,
-  humidity: 0
+const sensor = ref({
+  humidity: 0,
+  temperature: 0
 });
 
 const getSensorData = async () => {
   setInterval( async() => {
     const { data } = await sensorApi.get('');
+    sensor.value = data;
     console.log( data );
   } , 1000);
 };
-
+getSensorData();
 </script>
 
 <template>
 <div class="container text-light">
   <div class="row">
     <div class="col-12">
-      <h1>{{ data.temperature }} °C</h1>
-      <h1>{{ data.humidity }} %</h1>
+      <h1>{{ sensor.temperature }} °C</h1>
+      <h1>{{ sensor.humidity }} %</h1>
     </div>
   </div>
 </div>
